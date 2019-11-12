344ce40ea414be354161c2f0217ec1b2e74d30f9
Cody Williams <will1092@mail.nmc.edu>
2019-11-11 20:51:29 -0500

    Added SetTheme method

--- TextBasedRoulette/Program.cs
+++ TextBasedRoulette/Program.cs
@@ -11,6 +11,40 @@ namespace TextBasedRoulette
         static void Main(string[] args)
         {
 
+            SetTheme();
+
+            DisplayContinuePrompt();
         }
+
+        private static void SetTheme()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+
+        #region HELPER METHODS
+
+        /// <summary>
+        /// display continue prompt
+        /// </summary>
+        static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// display screen header
+        /// </summary>
+        static void DisplayScreenHeader(string headerText)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + headerText);
+            Console.WriteLine();
+        }
+
+        #endregion
     }
 }
