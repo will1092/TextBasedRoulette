0c07bc796d7c5c50837b9875878f48820165dd41
will1092 <57112446+will1092@users.noreply.github.com>
2019-11-19 23:56:12 -0500

    Added Results class, instantiated objects, properties to objects, objects to list

--- TextBasedRoulette/Program.cs
+++ TextBasedRoulette/Program.cs
@@ -25,21 +25,13 @@ namespace TextBasedRoulette
 
             DisplayMainMenu();
 
+
             DisplayContinuePrompt();
         }
 
 
 
-    class Result
-    {
-        public int number { get; set; }
-        public string oddity { get; set; }
-        public string color { get; set; }
-        public string half { get; set; }
-        public string column { get; set; }
-        public string dozen { get; set; }
 
-    }
 
         #region PROGRAM METHODS
 
@@ -53,7 +45,54 @@ namespace TextBasedRoulette
         //        object from the Result class
         public static void InstantiateResults()
         {
-            Result number1 = new Result();
+            List<Result> results = new List<Result>();
+
+            //
+            // instantiate results
+            //
+            #region NEWRESULTS
+            Result number1 = newResult();
+            Result number2 = newResult();
+            Result number3 = newResult();
+            Result number4 = newResult();
+            Result number5 = newResult();
+            Result number6 = newResult();
+            Result number7 = newResult();
+            Result number8 = newResult();
+            Result number9 = newResult();
+            Result number10 = newResult();
+            Result number11 = newResult();
+            Result number12 = newResult();
+            Result number13 = newResult();
+            Result number14 = newResult();
+            Result number15 = newResult();
+            Result number16 = newResult();
+            Result number17 = newResult();
+            Result number18 = newResult();
+            Result number19 = newResult();
+            Result number20 = newResult();
+            Result number21 = newResult();
+            Result number22 = newResult();
+            Result number23 = newResult();
+            Result number24 = newResult();
+            Result number25 = newResult();
+            Result number26 = newResult();
+            Result number27 = newResult();
+            Result number28 = newResult();
+            Result number29 = newResult();
+            Result number30 = newResult();
+            Result number31 = newResult();
+            Result number32 = newResult();
+            Result number33 = newResult();
+            Result number34 = newResult();
+            Result number35 = newResult();
+            Result number36 = newResult();
+            #endregion NEWRESULTS
+
+            //
+            // set results property values
+            //
+            #region RESULTSPROPERTIES
             number1.number = 1;
             number1.oddity = "odd";
             number1.color = "red";
@@ -61,16 +100,293 @@ namespace TextBasedRoulette
             number1.column = "first";
             number1.dozen = "first";
 
-            Result number2 = new Result();
             number2.number = 2;
             number2.oddity = "even";
             number2.color = "black";
             number2.half = "first";
-            number2.column = "first";
+            number2.column = "second";
             number2.dozen = "first";
 
+            number3.number = 3;
+            number3.oddity = "odd";
+            number3.color = "red";
+            number3.half = "first";
+            number3.column = "third";
+            number3.dozen = "first";
 
+            number4.number = 4;
+            number4.oddity = "even";
+            number4.color = "black";
+            number4.half = "first";
+            number4.column = "first";
+            number4.dozen = "first";
 
+            number5.number = 5;
+            number5.oddity = "odd";
+            number5.color = "red";
+            number5.half = "first";
+            number5.column = "second";
+            number5.dozen = "first";
+
+            number6.number = 6;
+            number6.oddity = "even";
+            number6.color = "black";
+            number6.half = "first";
+            number6.column = "third";
+            number6.dozen = "first";
+
+            number7.number = 7;
+            number7.oddity = "odd";
+            number7.color = "red";
+            number7.half = "first";
+            number7.column = "first";
+            number7.dozen = "first";
+
+            number8.number = 8;
+            number8.oddity = "even";
+            number8.color = "black";
+            number8.half = "first";
+            number8.column = "second";
+            number8.dozen = "first";
+
+            number9.number = 9;
+            number9.oddity = "odd";
+            number9.color = "red";
+            number9.half = "first";
+            number9.column = "third";
+            number9.dozen = "first";
+
+            number10.number = 10;
+            number10.oddity = "even";
+            number10.color = "black";
+            number10.half = "first";
+            number10.column = "first";
+            number10.dozen = "first";
+
+            number11.number = 11;
+            number11.oddity = "odd";
+            number11.color = "black";
+            number11.half = "first";
+            number11.column = "second";
+            number11.dozen = "first";
+
+            number12.number = 12;
+            number12.oddity = "even";
+            number12.color = "red";
+            number12.half = "first";
+            number12.column = "third";
+            number12.dozen = "first";
+
+            number13.number = 13;
+            number13.oddity = "odd";
+            number13.color = "black";
+            number13.half = "first";
+            number13.column = "first";
+            number13.dozen = "second";
+
+            number14.number = 14;
+            number14.oddity = "even";
+            number14.color = "red";
+            number14.half = "first";
+            number14.column = "second";
+            number14.dozen = "second";
+
+            number15.number = 15;
+            number15.oddity = "odd";
+            number15.color = "black";
+            number15.half = "first";
+            number15.column = "third";
+            number15.dozen = "second";
+
+            number16.number = 16;
+            number16.oddity = "even";
+            number16.color = "red";
+            number16.half = "first";
+            number16.column = "first";
+            number16.dozen = "second";
+
+            number17.number = 17;
+            number17.oddity = "odd";
+            number17.color = "black";
+            number17.half = "first";
+            number17.column = "second";
+            number17.dozen = "second";
+
+            number18.number = 18;
+            number18.oddity = "even";
+            number18.color = "red";
+            number18.half = "first";
+            number18.column = "third";
+            number18.dozen = "second";
+
+            number19.number = 19;
+            number19.oddity = "odd";
+            number19.color = "red";
+            number19.half = "first";
+            number19.column = "first";
+            number19.dozen = "second";
+
+            number20.number = 20;
+            number20.oddity = "even";
+            number20.color = "black";
+            number20.half = "second";
+            number20.column = "second";
+            number20.dozen = "second";
+
+            number21.number = 21;
+            number21.oddity = "odd";
+            number21.color = "red";
+            number21.half = "second";
+            number21.column = "third";
+            number21.dozen = "second";
+
+            number22.number = 22;
+            number22.oddity = "even";
+            number22.color = "black";
+            number22.half = "second";
+            number22.column = "first";
+            number22.dozen = "second";
+
+            number23.number = 23;
+            number23.oddity = "odd";
+            number23.color = "red";
+            number23.half = "second";
+            number23.column = "second";
+            number23.dozen = "second";
+
+            number24.number = 24;
+            number24.oddity = "even";
+            number24.color = "black";
+            number24.half = "second";
+            number24.column = "third";
+            number24.dozen = "second";
+
+            number25.number = 25;
+            number25.oddity = "odd";
+            number25.color = "red";
+            number25.half = "second";
+            number25.column = "first";
+            number25.dozen = "third";
+
+            number26.number = 26;
+            number26.oddity = "even";
+            number26.color = "black";
+            number26.half = "second";
+            number26.column = "second";
+            number26.dozen = "third";
+
+            number27.number = 27;
+            number27.oddity = "odd";
+            number27.color = "red";
+            number27.half = "second";
+            number27.column = "third";
+            number27.dozen = "third";
+
+            number28.number = 28;
+            number28.oddity = "even";
+            number28.color = "black";
+            number28.half = "second";
+            number28.column = "first";
+            number28.dozen = "third";
+
+            number29.number = 29;
+            number29.oddity = "odd";
+            number29.color = "black";
+            number29.half = "second";
+            number29.column = "second";
+            number29.dozen = "third";
+
+            number30.number = 30;
+            number30.oddity = "even";
+            number30.color = "red";
+            number30.half = "second";
+            number30.column = "third";
+            number30.dozen = "third";
+
+            number31.number = 31;
+            number31.oddity = "odd";
+            number31.color = "black";
+            number31.half = "second";
+            number31.column = "first";
+            number31.dozen = "third";
+
+            number32.number = 32;
+            number32.oddity = "even";
+            number32.color = "red";
+            number32.half = "second";
+            number32.column = "second";
+            number32.dozen = "third";
+
+            number33.number = 33;
+            number33.oddity = "odd";
+            number33.color = "black";
+            number33.half = "second";
+            number33.column = "third";
+            number33.dozen = "third";
+
+            number34.number = 34;
+            number34.oddity = "even";
+            number34.color = "red";
+            number34.half = "second";
+            number34.column = "first";
+            number34.dozen = "third";
+
+            number35.number = 35;
+            number35.oddity = "odd";
+            number35.color = "black";
+            number35.half = "second";
+            number35.column = "second";
+            number35.dozen = "third";
+
+            number36.number = 36;
+            number36.oddity = "even";
+            number36.color = "red";
+            number36.half = "second";
+            number36.column = "third";
+            number36.dozen = "third";
+            #endregion RESULTSPROPERTIES
+
+            //
+            // add results to the list
+            //
+            #region ADDRESULTSTOLIST
+            results.Add(number1);
+            results.Add(number2);
+            results.Add(number3);
+            results.Add(number4);
+            results.Add(number5);
+            results.Add(number6);
+            results.Add(number7);
+            results.Add(number8);
+            results.Add(number9);
+            results.Add(number10);
+            results.Add(number11);
+            results.Add(number12);
+            results.Add(number13);
+            results.Add(number14);
+            results.Add(number15);
+            results.Add(number16);
+            results.Add(number17);
+            results.Add(number18);
+            results.Add(number19);
+            results.Add(number20);
+            results.Add(number21);
+            results.Add(number22);
+            results.Add(number23);
+            results.Add(number24);
+            results.Add(number25);
+            results.Add(number26);
+            results.Add(number27);
+            results.Add(number28);
+            results.Add(number29);
+            results.Add(number30);
+            results.Add(number31);
+            results.Add(number32);
+            results.Add(number33);
+            results.Add(number34);
+            results.Add(number35);
+            results.Add(number36);
+            #endregion ADDRESULTSTOLIST
         }
 
         private static void DisplayMainMenu()
---
+++ TextBasedRoulette/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRoulette
+{
+    class Result
+    {
+        private int _number;
+        private string _oddity;
+        private string _color;
+        private string _half;
+        private string _column;
+        private string _dozen;
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public string Oddity
+        {
+            get { return _oddity; }
+            set { _oddity = value; }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public string Half
+        {
+            get { return _half; }
+            set { _half = value; }
+        }
+
+        public string Column
+        {
+            get { return _column; }
+            set { _column = value; }
+        }
+
+        public string Dozen
+        {
+            get { return _dozen; }
+            set { _dozen = value; }
+        }
+
+        public Result()
+        {
+
+        }
+    }
+}
